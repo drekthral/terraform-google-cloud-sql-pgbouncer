bd9522ae83c43abd040914277c8ba2cec38569a5
drekthral <jirka.zavadil98@gmail.com>
2022-11-17 00:24:13 +0100

    Update outputs.tf

--- modules/pgbouncer_cloud_init/outputs.tf
+++ modules/pgbouncer_cloud_init/outputs.tf
@@ -2,3 +2,29 @@ output "cloud_config" {
   description = "Cloud-init config to set up and serve PgBouncer."
   value       = data.cloudinit_config.cloud_config.rendered
 }
+
+output "instance_name" {
+  description = "The name for the PgBouncer instance."
+  value       = module.pgbouncer.instance_name
+}
+
+output "private_ip_address" {
+  description = "The first private IPv4 address assigned to the PgBouncer instance."
+  value       = module.pgbouncer.private_ip_address
+}
+
+output "public_ip_address" {
+  description = "The first public IPv4 address assigned for the PgBouncer instance."
+  value       = module.pgbouncer.public_ip_address
+}
+
+output "port" {
+  description = "The port number PgBouncer listens on."
+  value       = module.pgbouncer.port
+}
+
+output "database_url" {
+  description = "The database URL for connecting to PgBouncer."
+  value       = "postgres://${var.db_user}:${var.db_password}@${module.pgbouncer.public_ip_address}:${module.pgbouncer.port}/${var.db_name}"
+  sensitive   = true
+}
