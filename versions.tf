e84b5dbc03333e9f5faf48e50c2e40ff78ae2b88
drekthral <jirka.zavadil98@gmail.com>
2022-11-16 23:06:20 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,14 +1,12 @@
 terraform {
   required_version = ">= 0.13.0"
   required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.43.0"
-    }
+    google = ">= 3.5"
     null   = ">= 3.0"
   }
 }
 
 provider "google" {
-  
+  project = "lektory-prod"
+  zone = "europe-west1"
 }
