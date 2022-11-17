9640a9be4ef79b4c84dff5356095b43e2f1cf1ff
drekthral <jirka.zavadil98@gmail.com>
2022-11-17 12:55:29 +0100

    Update main.tf

--- modules/pgbouncer_cloud_init/main.tf
+++ modules/pgbouncer_cloud_init/main.tf
@@ -19,18 +19,6 @@ locals {
     }
   )
 }
-locals {
-  region = join("-", slice(split("-", var.zone), 0, 2))
-}
-resource "random_id" "suffix" {
-  byte_length = 5
-}
-
-data "google_compute_subnetwork" "subnet" {
-  project = var.project
-  name    = var.subnetwork_name
-  region  = local.region
-}
 
 data "template_file" "cloud_config" {
   template = file("${path.module}/templates/cloud-init.yaml.tmpl")
@@ -52,49 +40,3 @@ data "cloudinit_config" "cloud_config" {
   }
 }
 
-/* PgBouncer ---------------------------------------------------------------- */
-
-resource "google_compute_address" "pgbouncer" {
-  project      = var.project
-  region       = local.region
-  name         = "ip-pgbouncer-${random_id.suffix.hex}"
-  network_tier = "PREMIUM"
-}
-
-module "pgbouncer" {
-  source = "../.."
-
-  project           = var.project
-  name              = "vm-pgbouncer-${random_id.suffix.hex}"
-  zone              = var.zone
-  subnetwork        = var.subnetwork_name
-  public_ip_address = google_compute_address.pgbouncer.address
-  tags              = ["pgbouncer"]
-
-  disable_service_account = true
-
-  port          = 25128
-  database_host = module.db.private_ip_address
-
-  users = [
-    { name = var.db_user, password = var.db_password },
-  ]
-
-  module_depends_on = [module.db]
-}
-
-/* Firewall ----------------------------------------------------------------- */
-
-resource "google_compute_firewall" "pgbouncer" {
-  name    = "${var.network_name}-ingress-pgbouncer-${random_id.suffix.hex}"
-  project = var.project
-  network = var.network_name
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["pgbouncer"]
-
-  allow {
-    protocol = "tcp"
-    ports    = [module.pgbouncer.port]
-  }
-}
